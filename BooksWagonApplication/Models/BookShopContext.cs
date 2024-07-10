33bc69eb9ef7c53e3aa4715b306f5dc5669fa78d
sureshreddy2 <159027576+sureshreddy2@users.noreply.github.com>
2024-07-10 11:54:20 +0530

    Update BookShopContext.cs

--- BooksWagonApplication/Models/BookShopContext.cs
+++ BooksWagonApplication/Models/BookShopContext.cs
@@ -24,7 +24,8 @@ namespace BooksWagonApplication.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=RAMYASRI\\SQLEXPRESS; Database=Bookswagon;Trusted_Connection=True;");
+               optionsBuilder.UseNpgsql("Host=13.233.244.236;Port=5432;Database=bookswagon;Username=suresh;Password=12345");
+            }
             }
         }
 
